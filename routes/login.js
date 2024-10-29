129a63c6835c773312c27d3fdcf4a3d5a81f643d
Linngeshwar <lingaeshrajan@gmail.com>
2024-10-30 00:00:28 +0530

    [add] authorization added

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,22 @@
 const express = require('express');
+require('dotenv').config();
+const bcrypt = require('bcryptjs');
+const pool = require('../util/connectdb');
 const LoginRouter = express.Router();
 
-LoginRouter.post('/',(req,res)=>{
+LoginRouter.post('/',async (req,res)=>{
     const username = req.body.username; 
     const password = req.body.password;
-    const user = {
-        username: username,
-        password: password
-    }
-    res.json(user);
+    const result = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+    const user = result.rows[0];
+    const passwordFromDb = user.password;
+    bcrypt.compare(password, passwordFromDb, (err, result) => {
+        if (result) {
+            res.send('Login successful');
+        } else {
+            res.send('Invalid username or password');
+        }
+    });
 })
 
 module.exports = LoginRouter;
